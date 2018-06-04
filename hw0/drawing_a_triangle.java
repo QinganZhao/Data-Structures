12628f4779e4c1eee5fd0e53a176f17ceea0fdb4
QinganZhao <qingan_zhao@berkeley.edu>
2018-06-04 16:38:23 +0800

    my first java program:)

---
+++ hw0/drawing_a_triangle.java
@@ -0,0 +1,9 @@
+public class drawing_a_triangle{
+	public static void main(String[] args){
+		String star = "*";
+		for (int i = 0; i<5; i++){
+			System.out.println(star);
+			star = star + "*";
+		}
+	}
+}
